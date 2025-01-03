08d40f3e4b925e3ba924524db4d80062809ff31c
manshikandu <manshikandu@gmail.com>
2025-01-03 20:19:24 +0545

    1.authroutes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,10 @@
 import express from "express"
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 
 app.use("/api/v1/auth",authRoutes)
 
-app.get("/api/v1/signup",(req,res) => {
-    res.send("Signup route")
-})
 
-app.get("/api/v1/login",(req,res) => {
-    res.send("Login route")
-})
-
-app.get("/api/v1/logout",(req,res) => {
-    res.send("Logout route")
-})
 
 app.listen(5000,() => {
  console.log("Server started at http://localhost:5000");
