a251bfdc841e796da59ed5219423b48c5b7e1200
manshikandu <manshikandu@gmail.com>
2025-01-03 20:20:22 +0545

    serverjs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js";
 const app = express();
 
-app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/auth",authRoutes);
 
 
 
