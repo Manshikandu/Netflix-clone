30d5eb27a32a3b606e30b9ec7b7bd5751ae482e6
manshikandu <manshikandu@gmail.com>
2025-01-07 20:35:59 +0545

    4.movie.route

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getMovieDetails, getMoviesByCategory, getMovieTrailers, getSimilarMovies, getTrendingMovie } from "../controllers/movie.controller.js"
+import { getMovieDetails, getMoviesByCategory, getMovieTrailers, getSimilarMovies, getTrendingMovie } from "../controllers/movie.controller.js";
 const router = express.Router();
 
 router.get("/trending",getTrendingMovie)
