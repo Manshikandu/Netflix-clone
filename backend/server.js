a57239e1cea1a09c55e1d637474e856f7c076463
manshikandu <manshikandu@gmail.com>
2025-01-07 17:02:56 +0545

    serverjs

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
 import { ENV_VARS } from "./config/enVars.js";
 import { connectDB } from "./config/db.js";
 //import { ENV_VARS } from "./config/enVars.js";
@@ -8,6 +9,7 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 app.use(express.json()); //allow us to parse req.body object
 app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/movie",movieRoutes );
 
 
 app.listen(PORT,() => {
